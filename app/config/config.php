45114cc994233142c9f4f2357e9abf5e23ac7184
arberen <contact@arberen.com>
2023-01-22 22:23:57 +0100

    mise en prod

--- app/config/config.php
+++ app/config/config.php
@@ -1,14 +1,14 @@
 <?php
   // DB Params
   define('DB_HOST', 'localhost');
-  define('DB_USER', 'root');
-  define('DB_PASS', '123456');
-  define('DB_NAME', 'studi_kgb');
+  define('DB_USER', 'khmebzsv_studikgbcya');
+  define('DB_PASS', 'SzNOBPHgtr1819!');
+  define('DB_NAME', 'khmebzsv_studi_kgb');
 
   // App Root
   define('APPROOT', dirname(dirname(__FILE__)));
   // URL Root
-  define('URLROOT', 'http://localhost/datakgb');
+  define('URLROOT', 'https://studikgb.yasarweb.fr');
   // Site Name
   define('SITENAME', 'DataKGB');
   // App Version
